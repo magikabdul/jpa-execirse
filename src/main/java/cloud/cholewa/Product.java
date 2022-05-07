56d994fd7c62bba544b8f13590b7717822f8f3b4
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-05-07 18:45:16 +0200

    [LAB] - first entity and project configuration

---
+++ src/main/java/cloud/cholewa/Product.java
@@ -0,0 +1,17 @@
+package cloud.cholewa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
---
+++ src/test/java/cloud/cholewa/LabTests.java
@@ -0,0 +1,14 @@
+package cloud.cholewa;
+
+import org.junit.jupiter.api.Test;
+
+import static cloud.cholewa.TestUtilities.run;
+
+public class LabTests {
+
+    @Test
+    void test() {
+
+        run(entityManager -> {});
+    }
+}
